4747a681da5374cc7bb600311b196ab713872a07
Marko <heldmarko@gmail.com>
2021-09-29 17:12:56 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,91 @@
+//**
+// author: Marko Held 
+// sources
+//**
+
+#include <Arduino.h>
+
+
+int TEMP_PIN_A = 14;
+int TEMP_PIN_B = 15;
+
+unsigned long CURRENTMILLIS;
+unsigned long PREVIOUSMILLIS;
+unsigned int tempratureInterval = 500; //ms
+unsigned long ntc1LastCheck;
+unsigned long ntc2LastCheck;
+float tempA;
+float tempB;
+float array{0};
+float temprature(int pin);
+void log();
+
+void setup() {
+  Serial.begin (9600);
+  
+
+}
+
+void loop() {
+  CURRENTMILLIS = millis();
+  if (CURRENTMILLIS - ntc1LastCheck>= tempratureInterval){      //reading NTC TEMP
+    tempA = temprature(TEMP_PIN_A);
+    tempB = temprature(TEMP_PIN_B);
+
+    ntc1LastCheck = CURRENTMILLIS;
+
+    log();
+    //Serial.print("tempratureA");
+    //Serial.print(",");
+    //Serial.print(tempA);
+    //Serial.print(",");
+    //Serial.println();
+    //Serial.print("time,");
+    //Serial.print(CURRENTMILLIS);
+
+  }
+  //if (CURRENTMILLIS - ntc2LastCheck >= tempratureInterval){
+   // tempB = temprature(TEMP_PIN_B);
+   // ntc2LastCheck = CURRENTMILLIS;
+    //Serial.print("tempratureB");
+    //Serial.print(",");
+    //Serial.print(tempB);
+    //Serial.print(",");
+    //Serial.println();
+    //Serial.print("time  ");
+    //Serial.print(CURRENTMILLIS);
+    
+  
+}
+
+
+float temprature(int pin){
+  int _pin = pin;
+  int Vo;
+  float logR2, R2, T;
+  float R1 = 10000; 
+  float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07; //constants for ntc
+
+  
+    Vo = analogRead(_pin);
+    R2 = R1 * (1023.0 / (float)Vo - 1.0);
+    logR2 = log(R2);
+    T = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
+    T = T - 273.15;
+    
+    return T;
+
+}
+
+void log(){ //designed for excel data stream
+    Serial.print("tempratureA");
+    Serial.print(",");
+    Serial.print(tempA);
+    Serial.print(",");
+    Serial.print("tempratureB");
+    Serial.print(",");
+    Serial.print(tempB);
+    Serial.print(",");
+    Serial.println();
+  return;
+}
